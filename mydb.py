1b7d293bf8adcd8b65c7cb43e1df9e16e0e28b13
GiftVivien <magiftvivien@gmail.com>
2024-03-18 17:26:04 +0000

    Initial commit

---
+++ mydb.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password123'
+)
+    
+
+
+# prepare cursor object
+
+cursorObject  = dataBase.cursor()
+
+
+#create a database
+
+cursorObject.execute("CREATE DATABASE Gift")
+
+print("ALL DONE!")
+
